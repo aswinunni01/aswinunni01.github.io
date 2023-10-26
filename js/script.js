85d9818f4837058ba34f88e30c436f961a7cbff2
Aswin Unnikrishnan <aswinunni01@gmail.com>
2023-10-26 23:01:11 +0530

    Add scroll to top

--- js/script.js
+++ js/script.js
@@ -5,5 +5,14 @@ if (document.getElementById('my-work-link')) {
 }
 addEventListener("scroll", (event) => {
 	document.getElementById('navbar').style.opacity=1-document.documentElement.scrollTop/400
-	
 });
+
+let mybutton = document.getElementById("scrollbtn")
+addEventListener("scroll", (event) => {
+	mybutton.style.display = "block"
+	document.getElementById('scrollbtn').style.opacity=document.documentElement.scrollTop/400
+})
+
+function topFunction(){
+	window.scrollTo({top: 0, behavior: 'smooth'});
+}
