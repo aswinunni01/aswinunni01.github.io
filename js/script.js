bed0a7e9628716046059804342cee51728edf5dc
Aswin Unnikrishnan <aswinunni01@gmail.com>
2023-10-26 23:29:55 +0530

    Added navigation hamburger

--- js/script.js
+++ js/script.js
@@ -5,6 +5,13 @@ if (document.getElementById('my-work-link')) {
 }
 addEventListener("scroll", (event) => {
 	document.getElementById('navbar').style.opacity=1-document.documentElement.scrollTop/400
+	
+	if (document.documentElement.scrollTop > 10) {
+		var dropdown = document.getElementById("myLinks");
+		if (dropdown.style.display === "grid") {
+			navHamburger(document.getElementsByClassName("nav-hamburger")[0])
+		}
+	}
 });
 
 let mybutton = document.getElementById("scrollbtn")
@@ -15,4 +22,14 @@ addEventListener("scroll", (event) => {
 
 function topFunction(){
 	window.scrollTo({top: 0, behavior: 'smooth'});
+}
+
+function navHamburger(x) {
+  x.classList.toggle("change");
+  var dropdown = document.getElementById("myLinks");
+  if (dropdown.style.display === "grid") {
+    dropdown.style.display = "none";
+  } else {
+	  dropdown.style.display = "grid";
+  }
 }
